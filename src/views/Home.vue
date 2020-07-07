0057fc89efa42d13b1aabe51ea86d3d5b94eec08
xFakir <490679728@qq.com>
2020-07-07 10:23:31 +0800

    a

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <el-container class="test">
+    <el-aside width="256px">
+      <side-menu></side-menu>
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    SideMenu: () => import("@/views/SideMenu")
   }
 };
 </script>
+
+<style>
+.test {
+  height: 100%;
+  background-color: #42b983;
+}
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  height: 100%;
+  color: #333;
+  text-align: center;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+}
+  body{
+    margin: 0;
+    padding: 0;
+  }
+</style>
